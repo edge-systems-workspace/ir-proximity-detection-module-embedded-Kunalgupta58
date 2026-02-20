0b8e7b1241b0c7511a1a53dfef669c28624b56dd
Kunal Gupta <kunal.gupta_cs.aiml24@gla.ac.in>
2026-02-20 15:30:39 +0530

    Define IR sensor pin and initialize sensor state
    Removed redundant TODO comments and defined IR_PIN.

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,12 @@
  * and detects obstacle presence.
  */
 
+ // TODO 1: Define IR sensor digital pin (Use pin 2)
+ #define IR_PIN 2
 
- // TODO 1:
- // Define IR sensor digital pin (Use pin 2)
+ // TODO 2: Create variable to store sensor state
+ int irState = 0;
 
- // TODO 2:
- // Create variable to store sensor state
 
 void setup() {
 
